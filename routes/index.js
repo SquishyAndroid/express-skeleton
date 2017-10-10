c9e5bd1ff3684b8fc4fe3ebd3470bca4f5fa4583
Brad Engelhardt <brad.engelhardt@gmail.com>
2017-10-10 08:53:38 -0700

    minor updates

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index.hbs', {
-    userName: 'Brad',
-    pageTitle: 'Home',
-    welcomeMessage: 'Welcome to my website!',
-  })
+    res.render('index.hbs', {
+        userName: 'Brad',
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to my node + express website!',
+    })
 });
 
 /* GET home page. */
 router.get('/about', function (req, res, next) {
-  res.render('about.hbs', {
-    userName: 'Brad',
-    pageTitle: 'About',
-  })
+    res.render('about.hbs', {
+        userName: 'Brad',
+        pageTitle: 'About',
+        bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    })
 });
 
 /* GET home page. */
 router.get('/contact', function (req, res, next) {
-  res.render('contact.hbs', {
-    userName: 'Brad',
-    pageTitle: 'Contact',
-  })
+    res.render('contact.hbs', {
+        userName: 'Brad',
+        pageTitle: 'Contact',
+    })
 });
 
 module.exports = router;
